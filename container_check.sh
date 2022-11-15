c720d0eb8cc2f93721cb43f4b57370948274296e
Roomy <cilpong@gmail.com>
2022-11-15 21:36:28 +0900

    test

--- container_check.sh
+++ container_check.sh
@@ -8,5 +8,7 @@ if [ -n "$NGINX_CONTAINER_ID"];
       docker run -d -p 80:80 --name nginx nginx:test
   else
       echo "nginx container not exist"
+      docker stop $NGINX_CONTAINER_ID
+      docker rm $NGINX_CONTAINER_ID
       docker run -d -p 80:80 --name nginx nginx:test
 fi
