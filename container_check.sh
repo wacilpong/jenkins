d81e9559b52a8702798ca8512b1929287d6643b0
Roomy <cilpong@gmail.com>
2022-11-15 21:23:25 +0900

    ADD test files

---
+++ container_check.sh
@@ -0,0 +1,12 @@
+NGINX_CONTAINER_ID=`docker ps -aq --filter 'name=nginx'`
+
+if [ -n "$NGINX_CONTAINER_ID"];
+  then
+      echo "nginx container exist"
+      docker stop $NGINX_CONTAINER_ID
+      docker rm $NGINX_CONTAINER_ID
+      docker run -d -p 80:80 --name nginx nginx:test
+  else
+      echo "nginx container not exist"
+      docker run -d -p 80:80 --name nginx nginx:test
+fi
